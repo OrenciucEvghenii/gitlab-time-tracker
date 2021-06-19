39957ca2d45f0ce59132ef1ec22364be95e20169
Evghenii Orenciuc <evghenii.orenciuc@bitingbit.com>
2021-06-19 23:27:48 +0200

    create dummy Tickets.vue component

---
+++ src/pages/Tickets.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-page class="flex column">
+    <q-list bordered separator>
+      <q-item v-for="ticket in tickets" :key="ticket.id" clickable v-ripple>
+        <q-item-section>
+          {{ ticket.name }}
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Tickets',
+  data() {
+    return {
+      tickets: []
+    }
+  },
+  mounted() {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
