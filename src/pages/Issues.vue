4ce250477d63b2dcb6a1564efe3f66c0c6c907ec
Evghenii Orenciuc <evghenii.orenciuc@bitingbit.com>
2021-06-20 01:17:31 +0200

    rename Tickets.vue to Issues.vue and load issues on component creation or route change

---
+++ src/pages/Issues.vue
@@ -0,0 +1,48 @@
+<template>
+  <q-page class="flex column">
+    <div>No. of Issues: {{issues.length}}</div>
+    <q-list v-if="issues.length" bordered separator>
+      <q-item v-for="issue in issues" :key="issue.id" clickable v-ripple>
+        <q-item-section>
+          {{ issue.title }}
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Issues',
+  data() {
+    return {
+      issues: []
+    }
+  },
+  watch: {
+    $route() {
+      this.loadIssues()
+    }
+  },
+  created() {
+    this.loadIssues()
+  },
+  methods: {
+    loadIssues() {
+      // FIXME: refactor. prototype
+
+      this.$gitlabApi.get(`/projects/${this.$route.params.id}/issues`)
+        .then(({ data }) => {
+          this.issues = data
+        })
+        .catch(error => {
+          console.error(error)
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
