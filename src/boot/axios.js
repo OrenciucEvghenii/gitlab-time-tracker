b9473f59d17d16ced1d65ef3477c6377ea5a65ff
Evghenii Orenciuc <evghenii.orenciuc@bitingbit.com>
2021-06-19 23:24:13 +0200

    create additional axios instance as gitlab api client

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
 import axios from 'axios'
 
+const gitlabApi = axios.create()
+
 Vue.prototype.$axios = axios
+Vue.prototype.$gitlabApi = gitlabApi
+
+export { axios, gitlabApi }
