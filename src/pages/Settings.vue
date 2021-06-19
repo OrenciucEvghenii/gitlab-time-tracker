8fb1c86f48a4d069a74e3129ee0dd34c6d991fb5
Evghenii Orenciuc <evghenii.orenciuc@bitingbit.com>
2021-06-19 21:10:17 +0200

    add gitlab url and gitlab access token input fields and methods to save them in local storage

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -1,12 +1,55 @@
 <template>
-  <q-page class="flex">
+  <q-page class="flex column">
     <div>Settings</div>
+
+    <div class="q-gutter-md">
+      <q-input v-model="gitlabUrl" label="GitLab URL" filled dense>
+        <template v-slot:after>
+          <q-btn @click="persistGitlabUrl" dense flat icon="save"/>
+        </template>
+      </q-input>
+
+      <q-input v-model="gitlabAccessToken" label="GitLab Access Token" filled dense>
+        <template v-slot:after>
+          <q-btn @click="persistGitlabAccessToken" dense flat icon="save"/>
+        </template>
+      </q-input>
+
+      <q-btn type="a" :href="gitlabCreateAccessTokenUrl" target="_blank" label="Create AT" no-caps/>
+    </div>
+
   </q-page>
 </template>
 
 <script>
+
+// gzeG--UEZBmbLnRpr5Vu
+
 export default {
-  name: 'Settings'
+  name: 'Settings',
+  data() {
+    return {
+      gitlabUrl: null,
+      gitlabAccessToken: null
+    }
+  },
+  computed: {
+    gitlabCreateAccessTokenUrl() {
+      return `${this.gitlabUrl}/-/profile/personal_access_tokens`
+    }
+  },
+  created() {
+    this.gitlabUrl = localStorage.gitlabUrl
+    this.gitlabAccessToken = localStorage.gitlabAccessToken
+  },
+  methods: {
+    persistGitlabUrl() {
+      localStorage.gitlabUrl = this.gitlabUrl
+    },
+    persistGitlabAccessToken() {
+      localStorage.gitlabAccessToken = this.gitlabAccessToken
+    }
+  }
 }
 </script>
 
