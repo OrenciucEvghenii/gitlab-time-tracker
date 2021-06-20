e349028c29d0a25a546f8394abd2bf9e1b2133d4
Evghenii Orenciuc <evghenii.orenciuc@bitingbit.com>
2021-06-20 14:45:49 +0200

    add grid container to dashboard and add 'Projects' card

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,6 +1,12 @@
 <template>
-  <q-page class="flex">
-    <router-link :to="{name: 'projects'}">Projects</router-link>
+  <q-page class="flex justify-center content-center">
+    <div class="q-gutter-lg">
+      <q-card @click="$router.push({ name: 'projects' })" class="cursor-pointer" flat bordered>
+        <q-card-section>
+          <div class="text-h6">Projects</div>
+        </q-card-section>
+      </q-card>
+    </div>
   </q-page>
 </template>
 
