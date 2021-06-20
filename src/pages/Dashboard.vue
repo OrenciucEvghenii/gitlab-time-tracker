fb361464ce526dbda610328ba5fcadcf308779de
Evghenii Orenciuc <evghenii.orenciuc@bitingbit.com>
2021-06-20 14:50:40 +0200

    fix flexbox container on dashboard and add 'Report' card to dashboard

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,17 +1,15 @@
 <template>
-  <q-page class="flex justify-center content-center">
-    <div class="q-gutter-lg">
-      <q-card @click="$router.push({ name: 'projects' })" class="cursor-pointer" flat bordered>
-        <q-card-section>
-          <div class="text-h6">Projects</div>
-        </q-card-section>
-      </q-card>
-      <q-card @click="$router.push({ name: 'projects' })" class="cursor-pointer" flat bordered>
-        <q-card-section>
-          <div class="text-h6">Projects</div>
-        </q-card-section>
-      </q-card>
-    </div>
+  <q-page class="flex justify-center content-center q-gutter-lg">
+    <q-card @click="$router.push({ name: 'projects' })" class="cursor-pointer" flat bordered>
+      <q-card-section>
+        <div class="text-h6">Projects</div>
+      </q-card-section>
+    </q-card>
+    <q-card @click="$router.push({ name: 'report' })" class="cursor-pointer" flat bordered>
+      <q-card-section>
+        <div class="text-h6">Report</div>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
