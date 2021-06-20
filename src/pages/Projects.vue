460d776e89bc69e62b9695fe7bb8acab74fd6105
Evghenii Orenciuc <evghenii.orenciuc@bitingbit.com>
2021-06-21 01:24:35 +0200

    add loading animation and project amount to Projects.vue

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,16 +1,26 @@
 <template>
-  <q-page class="flex column">
-    <q-list bordered separator>
-      <q-item v-for="project in projects"
-              :key="project.id"
-              :to="{ name: 'issues', params: { projectId: project.id }}"
-              clickable
-              v-ripple>
-        <q-item-section>
-          {{ project.name }}
-        </q-item-section>
-      </q-item>
-    </q-list>
+  <q-page class="flex">
+    <div class="col-grow">
+      <q-scroll-area style="height: 100%">
+        <q-item>
+          <q-item-section>No. of Projects: {{ projects.length }}</q-item-section>
+        </q-item>
+
+        <q-linear-progress v-if="projectsLoading" indeterminate/>
+
+        <q-list bordered separator>
+          <q-item v-for="project in projects"
+                  :key="project.id"
+                  :to="{ name: 'issues', params: { projectId: project.id }}"
+                  clickable
+                  v-ripple>
+            <q-item-section>
+              {{ project.name }}
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
+    </div>
   </q-page>
 </template>
 
@@ -19,17 +29,21 @@ export default {
   name: 'Projects',
   data() {
     return {
-      projects: []
+      projects: [],
+      projectsLoading: false
     }
   },
   created() {
     // FIXME: refactor. prototype
+    this.projectsLoading = true
     this.$gitlabApi.get('/projects?membership=true')
       .then(({ data }) => {
         this.projects = data
+        this.projectsLoading = false
       })
       .catch(error => {
         console.error(error)
+        this.projectsLoading = false
       })
   }
 }
