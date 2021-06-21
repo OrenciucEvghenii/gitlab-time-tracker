403864fafea39edf58900ea7e75fe142eb90ac55
Evghenii Orenciuc <evghenii.orenciuc@bitingbit.com>
2021-06-21 19:05:02 +0200

    extend dashboard 'Projects' card with list of pinned projects

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,26 +1,44 @@
 <template>
-  <q-page class="flex justify-center content-center q-gutter-lg dashboard">
-    <q-card @click="$router.push({ name: 'projects' })" class="cursor-pointer" flat bordered>
-      <q-card-section>
+  <q-page class="flex row justify-center content-center q-gutter-lg dashboard">
+
+    <q-card class="col-4 flex column dashboard-card" flat bordered>
+      <q-card-section @click="$router.push({ name: 'projects' })" class="cursor-pointer">
         <div class="text-h6">Projects</div>
       </q-card-section>
+
+      <ProjectList pinned-only/>
     </q-card>
-    <q-card @click="$router.push({ name: 'report' })" class="cursor-pointer" flat bordered>
-      <q-card-section>
+
+    <q-card class="col-4 dashboard-card" flat bordered>
+      <q-card-section @click="$router.push({ name: 'report' })" class="cursor-pointer ">
         <div class="text-h6">Report</div>
       </q-card-section>
+
+      <q-separator inset />
+
+      <q-card-section>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </q-card-section>
     </q-card>
+
   </q-page>
 </template>
 
 <script>
+import ProjectList from 'components/ProjectList'
 export default {
-  name: 'Dashboard'
+  name: 'Dashboard',
+  components: { ProjectList }
 }
 </script>
 
 <style lang="scss" scoped>
 .dashboard {
   min-height: 500px;
+}
+
+.dashboard-card {
+  height: 300px;
 }
 </style>
