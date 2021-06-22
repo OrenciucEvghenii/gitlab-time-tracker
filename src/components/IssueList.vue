6597fb2c5a74b1fc089c3af215cdcd92d364cdad
Evghenii Orenciuc <evghenii.orenciuc@bitingbit.com>
2021-06-22 11:06:03 +0200

    move issue list to separate component and insert it into Issues.vue component

--- src/components/IssueList.vue
+++ src/components/IssueList.vue
@@ -1,10 +1,84 @@
 <template>
+  <div class="col-grow">
+    <q-scroll-area style="height: 100%">
+      <header>
+        <slot name="header" :count="issues.length"></slot>
+      </header>
 
+      <q-linear-progress v-if="issuesLoading" size="xs" indeterminate/>
+
+      <q-list :bordered="bordered" separator>
+        <IssueListItem v-for="issue in issues"
+                       :key="issue.id"
+                       :issue="issue"
+                       @stateChanged="loadIssues"
+                       show-actions/>
+      </q-list>
+      <q-item v-if="!issues.length && !issuesLoading">
+        No issues found
+      </q-item>
+    </q-scroll-area>
+  </div>
 </template>
 
 <script>
+import IssueListItem from 'components/IssueListItem'
+
 export default {
-  name: 'IssueList'
+  name: 'IssueList',
+  components: { IssueListItem },
+  props: {
+    projectId: {},
+    issueState: {
+      type: String
+    },
+    bordered: {
+      type: Boolean
+    }
+  },
+  data() {
+    return {
+      issues: [],
+      issuesLoading: false
+    }
+  },
+  computed: {
+    issuesUrl() {
+      // FIXME: verry ineffective method. Optimize!
+      // FIXME: make pagination dynamically [https://docs.gitlab.com/ee/api/README.html#pagination]
+
+      let url = (this.projectId)
+        ? `/projects/${this.projectId}/issues?per_page=100`
+        : '/issues?scope=assigned_to_me&per_page=100'
+
+      url += this.issueState ? `&state=${this.issueState}` : ''
+
+      return url
+    }
+  },
+  watch: {
+    issueState() {
+      this.loadIssues()
+    }
+  },
+  created() {
+    this.loadIssues()
+  },
+  methods: {
+    loadIssues() {
+      this.issuesLoading = true
+
+      this.$gitlabApi.get(this.issuesUrl)
+        .then(({ data }) => {
+          this.issues = data
+          this.issuesLoading = false
+        })
+        .catch(error => {
+          console.error(error)
+          this.issuesLoading = false
+        })
+    }
+  }
 }
 </script>
 
--- src/pages/Issues.vue
+++ src/pages/Issues.vue
@@ -1,86 +1,36 @@
 <template>
   <q-page class="flex">
-    <div class="col-grow">
-      <q-scroll-area style="height: 100%">
-        <q-list bordered separator>
-          <q-item>
-            <q-item-section>No. of Issues: {{ issues.length }}</q-item-section>
-            <q-btn-toggle v-model="issueState"
-                          :options="issueStateOptions"
-                          @input="loadIssues"
-                          toggle-color="primary"
-                          flat/>
-          </q-item>
-
-          <q-linear-progress v-if="issuesLoading" indeterminate/>
-
-          <IssueListItem v-for="issue in issues"
-                         :key="issue.id"
-                         :issue="issue"
-                         @stateChanged="loadIssues"
-                         show-actions/>
-
-        </q-list>
-      </q-scroll-area>
-    </div>
+    <IssueList :project-id="projectId" :issue-state="issueState" bordered>
+      <template #header="{count}">
+        <q-item>
+          <q-item-section>No. of Issues: {{ count }}</q-item-section>
+          <q-btn-toggle v-model="issueState"
+                        :options="issueStateOptions"
+                        toggle-color="primary"
+                        flat/>
+        </q-item>
+      </template>
+    </IssueList>
   </q-page>
 </template>
 
 <script>
-import IssueListItem from 'components/IssueListItem'
+import IssueList from 'components/IssueList'
 
 export default {
   name: 'Issues',
-  components: { IssueListItem },
+  components: { IssueList },
   props: {
-    projectId: {},
-    hideActions: {
-      type: Boolean,
-      default: false
-    },
-    clickable: {
-      type: Boolean,
-      default: false
-    }
+    projectId: {}
   },
   data() {
     return {
-      issues: [],
-      issuesLoading: false,
       issueState: null,
       issueStateOptions: [
         { label: 'All', value: null },
-        { label: 'Opened', value: '&state=opened' },
-        { label: 'Closed', value: '&state=closed' }
+        { label: 'Opened', value: 'opened' },
+        { label: 'Closed', value: 'closed' }
       ]
-    }
-  },
-  created() {
-    this.loadIssues()
-  },
-  methods: {
-    loadIssues() {
-      // FIXME: verry ineffective method. Optimize!
-      // FIXME: make pagination dynamically [https://docs.gitlab.com/ee/api/README.html#pagination]
-
-      this.issuesLoading = true
-
-      const url = (this.projectId)
-        ? `/projects/${this.projectId}/issues?per_page=100${this.issueState || ''}`
-        : `/issues?scope=assigned_to_me&per_page=100${this.issueState || ''}`
-
-      this.$gitlabApi.get(url)
-        .then(({ data }) => {
-          this.issues = data.map(issue => {
-            return { loading: false, ...issue }
-          })
-          this.issuesLoading = false
-          this.$emit('issues-loaded', this.issues)
-        })
-        .catch(error => {
-          console.error(error)
-          this.issuesLoading = false
-        })
     }
   }
 }
